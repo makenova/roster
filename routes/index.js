32e9ffab432ddf839020268890cbe7529096af05
kernjackson <kern.jackson@gmail.com>
2014-07-12 16:57:24 -0500

    Added about page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var student = require('./student');
 var student_details = require('./student_details')
-	
+
 module.exports = function (app) {
   app.get('/', function(req, res) {
     res.render('index', { title: 'OKCoders Roster' });
   });
-  app.use('/roster', student);
-  app.use('/', student_details);
+  app.get('/about', function(req, res) {
+		res.render('about', { title: 'About OKCoders' });
+	});
+	app.use('/', student_details);
+	app.use('/roster', student);
+
 };
