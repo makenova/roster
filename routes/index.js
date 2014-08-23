7974251c762c3ce8e39fd7177e36fc60e03438f5
makenova <themarchmyth@gmail.com>
2014-08-23 11:21:34 -0500

    modeling sutff

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,25 @@ var classList = require('./class');
 var student = require('./student');
 var Student = require('../models/student');
 var Class = require('../models/class');
+var _ = require('lodash');
 
 module.exports = function (app) {
   app.get('/', function(req, res) {
-    res.render('index', { title: 'OKCoders Roster' });
+    var query = Class.find({}, {year:true, semester:true});
+    query.exec(function (err, classes) {
+      if (err) throw err;
+      var sortedClasses = sortClasses(classes);
+      console.log(sortedClasses);
+      res.render('index', { title: 'OKCoders Roster', classes: sortedClasses});
+    });
+  });
+
+  app.get('/admin', function(req, res) {
+    res.render('admin', { title: 'OKCoders Roster' });
   });
 
   // create a student
-  app.post('/', function(req, res) {
+  app.post('/admin', function(req, res) {
     var student = new Student(req.body);
     student.save(function (err) {
       if (err) next(err);
@@ -41,3 +52,18 @@ module.exports = function (app) {
   app.use('/class', classList);
   app.use('/student', student);
 };
+
+
+function sortClasses (classes) {
+  var result = [];
+
+  _.uniq(_.pluck(classes, 'year')).forEach(function (year) {
+    var yearObj = {
+      year: year,
+      semesters: _.pluck(_.where(classes, {year:year}), 'semester')
+    };
+    result.push(yearObj);
+  });
+
+  return(result);
+}
